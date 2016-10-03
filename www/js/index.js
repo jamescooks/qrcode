bb473696cde26c70a8e0b0de3f41d79339696a1f
jamescooks <jg072380@gmail.com>
2016-10-03 21:11:14 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var resultDiv;
+var resultDiv, inAppBRef;
 
 document.addEventListener("deviceready", init, false);
 
@@ -28,10 +28,22 @@ function init() {
 function startScan() {
     cordova.plugins.barcodeScanner.scan(
         function (result) {
-            var s = "Result: <a href='" + result.text + "' onClick='OpenLink(this);return false;'> " + result.text + "</a> <br/>" +
+            /*var s = "Result: <a href='" + result.text + "' onClick='OpenLink(this);return false;'> " + result.text + "</a> <br/>" +
             "Format: " + result.format + "<br/>" +
             "Cancelled: " + result.cancelled;
-            resultDiv.innerHTML = s;
+            resultDiv.innerHTML = s;*/
+            
+            var conState = checkConnection(); //alert(conState);
+            
+            if (conState == Connection.NONE)
+            {
+                //window.location="no_con.html";
+                alert('This app requires an internet connection to continue.');
+            }
+            else
+            {
+                OpenLink (result.text);
+            }
         },
         function (error) {
             alert("Scanning failed: " + error);
@@ -41,5 +53,52 @@ function startScan() {
 
 function OpenLink (durl)
 {
-    window.open(encodeURI(durl), '_blank', 'location=no,hidden=yes,closebuttoncaption=Done,toolbar=no');
+    //alert(durl);
+    //window.open(encodeURI(durl), '_self', 'location=no,hidden=yes,closebuttoncaption=Done,toolbar=no');
+    
+    inAppBRef = window.open(encodeURI(durl), '_blank', 'location=no');
+    
+    inAppBRef.addEventListener('loadstart', inAppBLoadStart);
+    inAppBRef.addEventListener('loadstop', inAppBLoadStop);
+    inAppBRef.addEventListener('loaderror', inAppBLoadError);
+    inAppBRef.addEventListener('exit', inAppBClose);
+}
+
+function checkConnection()
+{
+   var networkState = navigator.network.connection.type;
+   var states = {};
+   states[Connection.UNKNOWN]  = 'Unknown connection';
+   states[Connection.ETHERNET] = 'Ethernet connection';
+   states[Connection.WIFI]     = 'WiFi connection';
+   states[Connection.CELL_2G]  = 'Cell 2G connection';
+   states[Connection.CELL_3G]  = 'Cell 3G connection';
+   states[Connection.CELL_4G]  = 'Cell 4G connection';
+   states[Connection.NONE]     = 'No network connection';
+  
+   return networkState;
+}
+
+
+function inAppBLoadStart(event)
+{
+    navigator.notification.activityStart("Please Wait", "Its loading....");
+}
+
+function inAppBLoadStop(event)
+{
+	 navigator.notification.activityStop();
+}
+
+function inAppBLoadError(event)
+{
+     navigator.notification.activityStop();
+}
+
+function inAppBClose(event)
+{
+     inAppBRef.removeEventListener('loadstart', inAppBLoadStart);
+     inAppBRef.removeEventListener('loadstop', inAppBLoadStop);
+     inAppBRef.removeEventListener('loaderror', inAppBLoadError);
+     inAppBRef.removeEventListener('exit', inAppBClose);
 }
