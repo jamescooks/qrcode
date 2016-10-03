f06714d266a6bf46e5820733c4873437ef2b36b6
jamescooks <jg072380@gmail.com>
2016-10-03 19:30:26 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -16,38 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-/*var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};*/
-
 var resultDiv;
 
 document.addEventListener("deviceready", init, false);
@@ -60,7 +28,7 @@ function init() {
 function startScan() {
     cordova.plugins.barcodeScanner.scan(
         function (result) {
-            var s = "Result: " + result.text + "<br/>" +
+            var s = "Result: <a href='" + result.text + "' onClick='OpenLink();'> " + result.text + "</a> <br/>" +
             "Format: " + result.format + "<br/>" +
             "Cancelled: " + result.cancelled;
             resultDiv.innerHTML = s;
@@ -69,4 +37,9 @@ function startScan() {
             alert("Scanning failed: " + error);
         }
     );
+}
+
+function OpenLink (durl)
+{
+    window.open(encodeURI(durl), '_blank', 'location=no,hidden=yes,closebuttoncaption=Done,toolbar=no');
 }
